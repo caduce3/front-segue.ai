4bbb683cfa60ae49e209e0156ea8fde152584639
Carlos Eduardo <74209989+caduce3@users.noreply.github.com>
2024-11-14 23:21:42 -0300

    fix: retirando rotas que não existem

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,11 +4,8 @@ import { Dashboard } from './pages/app/dashboard/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 import { AuthLayout } from './pages/_layouts/auth'
 import { SignUp } from './pages/auth/sign-up'
-import { Players } from './pages/app/players/players'
 import { NotFound } from './pages/404'
-import { PlayersDetails } from './pages/app/players/players-details/players-details'
 import { Users } from './pages/app/users/users'
-import { RelatorioTrafego } from './pages/app/trafego/trafego'
 import Apresentation from './pages/auth/apresentation'
 
 export const router = createBrowserRouter([
@@ -18,10 +15,7 @@ export const router = createBrowserRouter([
         errorElement: <NotFound />,
         children: [
             {path: '/', element: <Dashboard />},
-            {path: '/players', element: <Players />},
-            {path: 'players/:id', element: <PlayersDetails />},
             {path: '/users', element: <Users />},
-            {path: '/trafego', element: <RelatorioTrafego />},
         ]
     },
     {
