a8f19e59b5d0a8e6ef454e25093d4fe14497e17b
Carlos Eduardo <74209989+caduce3@users.noreply.github.com>
2024-11-14 23:25:56 -0300

    fix: retirando arquivos inuteis

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,7 +5,6 @@ import { SignIn } from './pages/auth/sign-in'
 import { AuthLayout } from './pages/_layouts/auth'
 import { SignUp } from './pages/auth/sign-up'
 import { NotFound } from './pages/404'
-import { Users } from './pages/app/users/users'
 import Apresentation from './pages/auth/apresentation'
 
 export const router = createBrowserRouter([
@@ -15,7 +14,6 @@ export const router = createBrowserRouter([
         errorElement: <NotFound />,
         children: [
             {path: '/', element: <Dashboard />},
-            {path: '/users', element: <Users />},
         ]
     },
     {
