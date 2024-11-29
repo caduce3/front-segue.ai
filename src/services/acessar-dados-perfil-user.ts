c1cd11c9a057b9321227f77e829a5caac0bcef4c
Carlos Eduardo <74209989+caduce3@users.noreply.github.com>
2024-11-29 17:40:47 -0300

    fix: deixando em formatação padrão

--- src/services/acessar-dados-perfil-user.ts
+++ src/services/acessar-dados-perfil-user.ts
@@ -2,23 +2,20 @@ import { Igreja, UserEquipeDirigente } from "@/api/get-profile-user";
 
 // Função para processar os dados do perfil do usuário
 export function getUserProfileData(profileUser: Igreja | UserEquipeDirigente) {
-    let igrejaId = "";
-    let idUserEquipeDirigente = "";
-  
-    if (profileUser && typeof profileUser === "object") {
-      console.log("Dados recebidos:", profileUser);
-  
-      if ("cnpj" in profileUser) {
-        // O perfil é do tipo Igreja
-        igrejaId = profileUser.id;
-        idUserEquipeDirigente = profileUser.id;
-      } else if ("ano" in profileUser) {
-        // O perfil é do tipo EquipeDirigente
-        igrejaId = profileUser.igrejaId;
-        idUserEquipeDirigente = profileUser.id;
-      }
+  let igrejaId = "";
+  let idUserEquipeDirigente = "";
+
+  if (profileUser && typeof profileUser === "object") {
+    if ("cnpj" in profileUser) {
+      // O perfil é do tipo Igreja
+      igrejaId = profileUser.id;
+      idUserEquipeDirigente = profileUser.id;
+    } else if ("ano" in profileUser) {
+      // O perfil é do tipo EquipeDirigente
+      igrejaId = profileUser.igrejaId;
+      idUserEquipeDirigente = profileUser.id;
     }
-  
-    return { igrejaId, idUserEquipeDirigente };
   }
-  
+
+  return { igrejaId, idUserEquipeDirigente };
+}
