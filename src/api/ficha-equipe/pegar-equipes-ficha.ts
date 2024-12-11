3c8ee782e6d4c5fdaf54386c82758886db920274
Carlos Eduardo <74209989+caduce3@users.noreply.github.com>
2024-12-11 18:17:49 -0300

    feat: conexão com api para pegar as equipes da ficha

---
+++ src/api/ficha-equipe/pegar-equipes-ficha.ts
@@ -0,0 +1,78 @@
+import { api } from "@/lib/axios";
+
+export interface PegarEquipesFichaBody {
+  page: number;
+  igrejaId: string;
+  idUserEquipeDirigente: string;
+  fichaId: string;
+}
+
+export interface PegarEquipesFichaResponse {
+  totalItens: number;
+  totalPages: number;
+  currentPage: number;
+  equipesFichaList: {
+    id: string;
+    equipe:
+      | "ANIMACAO"
+      | "VIGILIA_PAROQUIAL"
+      | "LITURGIA"
+      | "CANTO"
+      | "ESTACIONAMENTO"
+      | "TAXI"
+      | "LANCHE"
+      | "COZINHA"
+      | "SALA"
+      | "FAXINA"
+      | "ED_PALESTRA"
+      | "ED_POS"
+      | "ED_MONTAGEM"
+      | "ED_FINANCAS"
+      | "ED_FICHAS"
+      | "CIRCULO"
+      | "GRAFICA"
+      | "MINI_MERCADO";
+    ano: string;
+    funcao: "EQUIPISTA" | "COORDENADOR" | "ED" | "APOIO";
+    avaliacao: "NEGATIVA" | "POSITIVA" | "NEUTRA";
+    observacoes: string;
+    createdAt: string;
+    updatedAt: string;
+    filiacaoPai: string;
+    fichaId: string;
+  }[];
+}
+
+export async function pegarEquipesFicha({
+  page,
+  igrejaId,
+  idUserEquipeDirigente,
+  fichaId,
+}: PegarEquipesFichaBody) {
+  try {
+    const token = localStorage.getItem("authToken");
+    if (!token) throw new Error("No token found");
+
+    const response = await api.post<PegarEquipesFichaResponse>(
+      "/pegar_equipes_ficha",
+      {
+        page,
+        igrejaId,
+        idUserEquipeDirigente,
+        fichaId,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      throw new Error(error.response.data?.message || "Erro de autenticação");
+    } else {
+      throw new Error("Erro ao conectar com o servidor");
+    }
+  }
+}
