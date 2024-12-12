fb1a639ffc66fa3e4d5d7f94130da99b87de3627
Carlos Eduardo <74209989+caduce3@users.noreply.github.com>
2024-12-12 19:36:07 -0300

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -26,7 +26,6 @@ export const router = createBrowserRouter([
         path: '/',
         element: <AuthLayout />,
         children: [
-            {path: '/', element: <Apresentation />},
             {path: '/apresentation', element: <Apresentation />},
             {path: '/sign-in', element: <SignIn />},
             {path: '/sign-up', element: <SignUp />}
